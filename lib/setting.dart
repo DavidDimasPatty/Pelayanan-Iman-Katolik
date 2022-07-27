3bbbf52a3b48e09cf0abe77a5a0fe946a9186966
unknown <daviddimas80@gmail.com>
2022-07-27 12:01:49 +0700

    add setting page

---
+++ lib/setting.dart
@@ -0,0 +1,89 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:pelayanan_iman_katolik/DatabaseFolder/mongodb.dart';
+import 'package:pelayanan_iman_katolik/login.dart';
+import 'DatabaseFolder/fireBase.dart';
+import 'homePage.dart';
+import 'tiketSaya.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class Settings extends StatelessWidget {
+  final name;
+  final email;
+  final idUser;
+  var dataUser;
+
+  //print('Download-Link: $urlDownload');
+
+  Settings(this.name, this.email, this.idUser);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+        ),
+        title: const Text('Profile'),
+        actions: <Widget>[],
+      ),
+      body: Center(child: Column(children: <Widget>[])),
+      bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
+            ),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              showUnselectedLabels: true,
+              unselectedItemColor: Colors.blue,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home, color: Color.fromARGB(255, 0, 0, 0)),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle_rounded,
+                      color: Color.fromARGB(255, 0, 0, 0)),
+                  label: "TiketKu",
+                )
+              ],
+              onTap: (index) {
+                if (index == 1) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => tiketSaya(name, email, idUser)),
+                  );
+                } else if (index == 0) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => HomePage(name, email, idUser)),
+                  );
+                }
+              },
+            ),
+          )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+          openCamera();
+        },
+        tooltip: 'Increment',
+        child: new Icon(Icons.camera_alt_rounded),
+      ),
+    );
+  }
+}
