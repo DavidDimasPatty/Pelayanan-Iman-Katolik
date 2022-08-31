4283ccaa0a2cb922f662ce45462f517afd89b541
unknown <daviddimas80@gmail.com>
2022-08-31 20:04:21 +0700

    add env

--- lib/DatabaseFolder/data.dart
+++ lib/DatabaseFolder/data.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 const MONGO_CONN_URL =
     "mongodb+srv://i18040:wearedead@cluster0.hw29l.mongodb.net/GerejaDB?retryWrites=true&w=majority";
+//const MONGO_CONN_URL = dotenv.env['email'].toString();
 const USER_COLLECTION = "user";
 const GEREJA_COLLECTION = "Gereja";
 const JADWAL_GEREJA_COLLECTION = "jadwalGereja";
