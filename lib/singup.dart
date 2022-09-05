5133edfc108debe05248c3aeb8650a3fe6a90ed0
unknown <daviddimas80@gmail.com>
2022-09-05 20:35:44 +0700

    sign up validation

--- lib/singup.dart
+++ lib/singup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:pelayanan_iman_katolik/DatabaseFolder/mongodb.dart';
 import 'package:pelayanan_iman_katolik/FadeAnimation.dart';
 import 'package:pelayanan_iman_katolik/login.dart';
@@ -12,99 +13,106 @@ class SignUp extends StatelessWidget {
   TextEditingController passwordController = new TextEditingController();
 
   checkSignUp(context) async {
-    var email = emailController.text;
-    bool emailValid = RegExp(
-            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-        .hasMatch(email);
-    var nama = nameController.text;
-    bool namaValid = RegExp(r'^[a-z A-Z,.\-]+$').hasMatch(nama);
-    if (namaValid == false) {
-      nameController.text = "";
+    if (nameController.text == "" ||
+        emailController.text == "" ||
+        passwordController.text == "" ||
+        repasswordController.text == "") {
+      Fluttertoast.showToast(
+          msg: "Mohon Mengisi Semua Data",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 2,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      emailController.clear();
+      passwordController.clear();
+    } else {
+      var email = emailController.text;
+      bool emailValid = RegExp(
+              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+          .hasMatch(email);
+      var nama = nameController.text;
+      bool namaValid = RegExp(r'^[a-z A-Z,.\-]+$').hasMatch(nama);
+      if (namaValid == false) {
+        nameController.text = "";
+        Fluttertoast.showToast(
+            msg: "Nama Lengkap Tidak Valid",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 2,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        emailController.clear();
+        passwordController.clear();
+      }
 
-      return showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('Nama lengkap tidak valid'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      );
-    }
+      if (passwordController.text != repasswordController.text) {
+        passwordController.text = "";
+        repasswordController.text = "";
+        Fluttertoast.showToast(
+            msg: "Password Tidak Sama",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 2,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        emailController.clear();
+        passwordController.clear();
+      }
+      if (emailValid == false) {
+        print("masuk");
+        emailController.text = "";
+        Fluttertoast.showToast(
+            msg: "Email Tidak Valid",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 2,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        emailController.clear();
+        passwordController.clear();
+      }
 
-    if (passwordController.text != repasswordController.text) {
-      passwordController.text = "";
-      repasswordController.text = "";
-      return showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('Password tidak sama'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      );
-    }
-    if (emailValid == false) {
-      print("masuk");
-      emailController.text = "";
-      return showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('Email tidak valid'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      );
-    }
+      if (emailValid == true &&
+          namaValid == true &&
+          passwordController.text == repasswordController.text) {
+        var add = await MongoDatabase.addUser(
+            nameController.text, emailController.text, passwordController.text);
 
-    if (emailValid == true &&
-        namaValid == true &&
-        passwordController.text == repasswordController.text) {
-      var add = await MongoDatabase.addUser(
-          nameController.text, emailController.text, passwordController.text);
-      print(add);
-      if (add == "nama") {
-        return showDialog<String>(
-          context: context,
-          builder: (BuildContext context) => AlertDialog(
-            title: const Text('Nama sudah digunakan'),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () => Navigator.pop(context, 'OK'),
-                child: const Text('OK'),
-              ),
-            ],
-          ),
-        );
-      } else if (add == "email") {
-        return showDialog<String>(
-          context: context,
-          builder: (BuildContext context) => AlertDialog(
-            title: const Text('Email sudah digunakan'),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () => Navigator.pop(context, 'OK'),
-                child: const Text('OK'),
-              ),
-            ],
-          ),
-        );
-      } else if (add == 'oke') {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => Login()),
-        );
+        if (add == nameController.text) {
+          nameController.text = "";
+          Fluttertoast.showToast(
+              msg: "Nama Sudah Digunakan",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.CENTER,
+              timeInSecForIosWeb: 2,
+              backgroundColor: Colors.red,
+              textColor: Colors.white,
+              fontSize: 16.0);
+          emailController.clear();
+          passwordController.clear();
+        } else if (add == emailController.text) {
+          emailController.text = "";
+          Fluttertoast.showToast(
+              msg: "Email Sudah Digunakan",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.CENTER,
+              timeInSecForIosWeb: 2,
+              backgroundColor: Colors.red,
+              textColor: Colors.white,
+              fontSize: 16.0);
+          emailController.clear();
+          passwordController.clear();
+        } else if (add == 'oke') {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => Login()),
+          );
+        }
       }
     }
   }
