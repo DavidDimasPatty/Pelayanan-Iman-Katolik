7930396a7619f4bf75f3ed2bfb70b9b15413193d
unknown <daviddimas80@gmail.com>
2023-01-26 17:42:59 +0700

    umum

--- lib/tiketSayaDetailKomuni.dart
+++ lib/tiketSayaDetailKomuni.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import 'package:pelayanan_iman_katolik/DatabaseFolder/mongodb.dart';
+import 'package:pelayanan_iman_katolik/agen/agenPage.dart';
+import 'package:pelayanan_iman_katolik/agen/messages.dart';
 import 'package:pelayanan_iman_katolik/profile.dart';
 import 'package:pelayanan_iman_katolik/tiketSaya.dart';
 
@@ -18,35 +21,56 @@ class tiketSayaDetailKomuni {
   tiketSayaDetailKomuni(this.names, this.emails, this.idUser, this.idKomuni,
       this.idGereja, this.idUserKomuni);
 
-  Future<List> callInfoBaptis(idBaptis) async {
-    tiket = await MongoDatabase.jadwalKomuni(idBaptis);
+  Future<List> callDb() async {
+    Messages msg = new Messages();
+    msg.addReceiver("agenPencarian");
+    msg.setContent([
+      ["cari Detail Jadwal Komuni"],
+      [idKomuni],
+      [idGereja]
+    ]);
+
+    await msg.send().then((res) async {
+      print("masuk");
+      print(await AgenPage().receiverTampilan());
+    });
+    await Future.delayed(Duration(seconds: 1));
+    tiket = await AgenPage().receiverTampilan();
+
     return tiket;
+    // tiket = await MongoDatabase.jadwalBaptis(idBaptis);
+    // return tiket;
   }
 
-  Future<List> callInfoGereja(idGereja) async {
-    namaGereja = await MongoDatabase.cariGereja(idGereja);
-    return namaGereja;
-  }
+  cancelDaftar(kapasitas, context) async {
+    Messages msg = new Messages();
+    msg.addReceiver("agenPencarian");
+    msg.setContent([
+      ["cancel Komuni"],
+      [idUserKomuni],
+      [idKomuni],
+      [kapasitas]
+    ]);
 
-  cancelDaftar(idMisa, context) async {
-    cancelKomuni = await MongoDatabase.cancelDaftarKomuni(idMisa);
+    await msg.send().then((res) async {
+      print("masuk");
+      print(await AgenPage().receiverTampilan());
+    });
+    await Future.delayed(Duration(seconds: 1));
+    cancelKomuni = await AgenPage().receiverTampilan();
     if (cancelKomuni == 'oke') {
-      return showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('Berhasil Membatalkan'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => tiketSaya(names, emails, idUser)),
-              ),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      );
+      Fluttertoast.showToast(
+          msg: "Berhasil Cancel Komuni",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 2,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      Navigator.pop(
+          context,
+          MaterialPageRoute(
+              builder: (context) => tiketSaya(names, emails, idUser)));
     }
   }
 
@@ -72,53 +96,68 @@ class tiketSayaDetailKomuni {
   }
 
   void showDialogBox(BuildContext context) async {
-    await callInfoBaptis(idKomuni);
-    await callInfoGereja(idGereja);
+    await callDb();
+    // await callInfoGereja(idGereja);
     showDialog<void>(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0))),
-              alignment: Alignment.center,
-              content: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        _getCloseButton(context),
-                        Text(
-                          "Detail Jadwal",
-                          style: TextStyle(
-                              color: Colors.blueAccent,
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.w300),
-                        ),
-                        Text('Waktu: ' +
-                            tiket[0]['jadwalBuka'].toString().substring(0, 19) +
-                            " s/d " +
-                            tiket[0]['jadwalTutup']
-                                .toString()
-                                .substring(0, 19)),
-                        Text('Nama Gereja: ' + namaGereja[0]['nama']),
-                        Text('Alamat Gereja: ' + namaGereja[0]['address']),
-                      ],
-                    )
-                  ]),
-              actions: <Widget>[
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      RaisedButton(
-                          child: Text('Batalkan Mendaftar'),
-                          textColor: Colors.white,
-                          color: Colors.blueAccent,
-                          onPressed: () async {
-                            await cancelDaftar(idUserKomuni, context);
-                          }), // button 1
-                    ])
-              ]);
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(32.0))),
+            alignment: Alignment.center,
+            content: FutureBuilder<List>(
+                future: callDb(),
+                builder: (context, AsyncSnapshot snapshot) {
+                  try {
+                    print(snapshot.data);
+                    return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              _getCloseButton(context),
+                              Text(
+                                "Detail Jadwal",
+                                style: TextStyle(
+                                    color: Colors.blueAccent,
+                                    fontSize: 24.0,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                              Text('Waktu: ' +
+                                  snapshot.data[0][0][0]['jadwalBuka']
+                                      .toString()
+                                      .substring(0, 19) +
+                                  " s/d " +
+                                  snapshot.data[0][0][0]['jadwalTutup']
+                                      .toString()
+                                      .substring(0, 19)),
+                              Text('Nama Gereja: ' +
+                                  snapshot.data[1][0][0]['nama']),
+                              Text('Alamat Gereja: ' +
+                                  snapshot.data[1][0][0]['address']),
+                            ],
+                          ),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                RaisedButton(
+                                    child: Text('Batalkan Mendaftar'),
+                                    textColor: Colors.white,
+                                    color: Colors.blueAccent,
+                                    onPressed: () async {
+                                      await cancelDaftar(
+                                          snapshot.data[0][0][0]['kapasitas'],
+                                          context);
+                                    }), // button 1
+                              ])
+                        ]);
+                  } catch (e) {
+                    print(e);
+                    return Center(child: CircularProgressIndicator());
+                  }
+                }),
+          );
         });
   }
 }
